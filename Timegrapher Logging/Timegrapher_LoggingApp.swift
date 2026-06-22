79ee3a1bcf52642ed474d55c9b60f1037ad81e98
Dan Ricks <DanRicks@hotmail.com>
2026-06-22 11:13:59 -0600

    Initial Commit

---
+++ Timegrapher Logging/Timegrapher_LoggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timegrapher_LoggingApp.swift
+//  Timegrapher Logging
+//
+//  Created by Dan on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Timegrapher_LoggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Timegrapher LoggingTests/Timegrapher_LoggingTests.swift
@@ -0,0 +1,16 @@
+//
+//  Timegrapher_LoggingTests.swift
+//  Timegrapher LoggingTests
+//
+//  Created by Dan on 6/22/26.
+//
+
+import Testing
+
+struct Timegrapher_LoggingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
